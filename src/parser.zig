c33d5f035e3b6f433a032550a1eb1a93433d3192
Calle Englund <git@notcalle.xyz>
2020-11-02 16:25:48 +0100

    Cleanup documentation

--- src/parser.zig
+++ src/parser.zig
@@ -28,26 +28,41 @@ usingnamespace @import("meta.zig");
 usingnamespace @import("result.zig");
 usingnamespace @import("string_parser.zig");
 
-/// Constructs a parser from a struct with a single `fn parse(Input)Result(T)`
+///
+/// Base type constructor for ParZig parsers
+///
+/// Constructs a parser from a partial parser.
+///
+/// Arguments:
+///     `P`: parser type struct
+///     `P.T`: value type of parse results
+///     `P.parse`: parser function `Input -> Result(T)`
+///
+/// Returns:
+///     `Parser{ .T = P.T }
+///
 pub fn Parser(comptime P: type) type {
     return struct {
         // Keep in sync with `meta.isParser`
         const Self = @This();
+
+        /// Value type of parse results
         pub const T = ParseResult(P).T;
+
         pub usingnamespace (P);
 
-        pub fn Alt(comptime alt: type) type {
-            return Parser(struct {
-                pub fn parse(input: Input) Result(T) {
-                    const r = Self.parse(input);
-                    if (.Some == r) return r;
-                    return alt.parse(input);
-                }
-            });
-        }
-
-        /// Functor `map` function. Constructs a parser that calls `map(T)U` to
-        /// transform a parse `Result(T)` to a parse `Result(U)`.
+        ///
+        /// Functor `map` combinator
+        ///
+        /// Constructs a parser that calls a function `f` to transform a parse
+        /// `Result(T)` to a parse `Result(U)`.
+        ///
+        /// Arguments:
+        ///     `f`: transform function `Self.T -> U`
+        ///
+        /// Returns:
+        ///     `Parser{ .T = U }`
+        ///
         pub fn Map(comptime U: type, comptime map: anytype) type {
             return Parser(struct {
                 pub fn parse(input: Input) Result(U) {
@@ -59,16 +74,51 @@ pub fn Parser(comptime P: type) type {
             });
         }
 
-        /// Constructs a parser that runs two parsers, returning the left result
-        /// when both are successful. If either parser fails, the leftmost
-        /// failure is returned.
-        pub fn SeqL(comptime next: type) type {
+        ///
+        /// Alternative sequence combinator `<|>`
+        ///
+        /// Constructs a parser that runs two parsers and returns the leftmost
+        /// successful result, or the rightmost failure reason.
+        ///
+        /// Arguments:
+        ///     `R`: right side parser
+        ///
+        /// Conditions:
+        ///     `Self.T == R.T`
+        ///
+        /// Returns:
+        ///     `Result(T)`
+        ///
+        pub fn Alt(comptime R: type) type {
+            return Parser(struct {
+                pub fn parse(input: Input) Result(T) {
+                    const r = Self.parse(input);
+                    if (.Some == r) return r;
+                    return R.parse(input);
+                }
+            });
+        }
+
+        ///
+        /// Left sequence combinator `<*`
+        ///
+        /// Constructs a parser that runs two parsers, returning the left
+        /// result when both are successful. If either parser fails, the
+        /// leftmost failure is returned.
+        ///
+        /// Arguments:
+        ///     `R`: right side parser
+        ///
+        /// Returns:
+        ///     `Parser{.T = Self.T}`
+        ///
+        pub fn SeqL(comptime R: type) type {
             return Parser(struct {
                 pub fn parse(input: Input) Result(T) {
                     switch (Self.parse(input)) {
                         .None => |r| return Result(T).fail(r),
                         .Some => |left| //
-                        switch (next.parse(left.tail)) {
+                        switch (R.parse(left.tail)) {
                             .None => |r| return Result(T).fail(r),
                             .Some => //
                             return Result(T).some(left.value, left.tail),
@@ -78,18 +128,28 @@ pub fn Parser(comptime P: type) type {
             });
         }
 
+        ///
+        /// Right sequence combinator `*>`
+        ///
         /// Constructs a parser that runs two parsers, returning the right
         /// result when both are successful. If either parser fails, the
-        /// leftmost failure is returned.
-        pub fn SeqR(comptime next: type) type {
-            const U = next.T;
+        /// leftmost failure is returned, wrapped as the right result.
+        ///
+        /// Arguments:
+        ///     `R`: right side parser
+        ///
+        /// Returns:
+        ///     `Parser{.T = R.T}`
+        ///
+        pub fn SeqR(comptime R: type) type {
+            const U = R.T;
 
             return Parser(struct {
                 pub fn parse(input: Input) Result(U) {
                     switch (Self.parse(input)) {
                         .None => |r| return Result(U).fail(r),
                         .Some => |left| //
-                        switch (next.parse(left.tail)) {
+                        switch (R.parse(left.tail)) {
                             .None => |r| return Result(U).fail(r),
                             .Some => |right| //
                             return Result(U).some(right.value, right.tail),
@@ -99,23 +159,81 @@ pub fn Parser(comptime P: type) type {
             });
         }
 
-        /// Monadic `bind`/`flatMap` function. Constructs a parser that calls
-        /// `map(T)Result(U)` to transform a parse `Result(T)` to a parse `Result(U)`.
-        pub fn Bind(comptime U: type, comptime map: anytype) type {
-
-            return Parser(struct {
-                pub fn parse(input: Input) Result(U) {
-                    switch (Self.parse(input)) {
-                        .Some => |r| return map(r.value),
-                        .None => |r| return Result(U).fail(r),
-                    }
-                }
-            });
+        ///
+        /// Monadic `bind` combinator `>>=`
+        ///
+        /// Constructs a parser that calls a function `f` to transform a parse
+        /// `Result(T)` to a parse `Result(U)`.
+        ///
+        /// Arguments:
+        ///     `U`: result value type
+        ///     `f`: transform function `Self.T -> Result(U)`
+        ///
+        /// Returns:
+        ///     `Parser{ .T = U }`
+        ///
+        pub fn Bind(comptime U: type, comptime f: anytype) type {
+            return Self.Map(Result(U), f).Join;
         }
+
+        ///
+        /// Monadic `join` combinator
+        ///
+        /// Constructs a parser that unwraps a nested result type
+        /// `Result(Result(U))`, to `Result(U)`.
+        ///
+        /// Arguments: none
+        ///
+        /// Conditions:
+        ///     `Self{.T = Result(U)}`
+        ///
+        /// Returns:
+        ///     `Parser{ .T = U }`
+        ///
+        pub const Join = Parser(struct {
+            pub fn parse(input: Input) Result(U) {
+                switch (Self.parse(input)) {
+                    .None => |r| return Result(U).fail(r),
+                    .Some => |r| return r,
+                }
+            }
+        });
     };
 }
 
-/// A parser that always fails
+///
+/// Monadic `pure` constructor
+///
+/// Constructs a parser that returns a constant successful result.
+///
+/// Arguments:
+///     `v`: parse result value
+///
+/// Returns:
+///     `Parser{ .T = @TypeOf(v) }`
+///
+pub fn Pure(comptime v: anytype) type {
+    const T = @TypeOf(v);
+
+    return Parser(struct {
+        pub fn parse(input: Input) Result(T) {
+            return Result(T).some(v, input);
+        }
+    });
+}
+
+///
+/// Constant failure reason constructor
+///
+/// Constructs a parser that always fails with the given reason.
+///
+/// Arguments:
+///     `T`: parse result value type
+///     `why`: opptional reason for failure
+///
+/// Returns:
+///     `Parser{ .T = T }`
+///
 pub fn Fail(comptime T: type, comptime why: ?Reason) type {
     return Parser(struct {
         pub fn parse(input: Input) Result(T) {
@@ -128,12 +246,10 @@ test "parse failure" {
     t.expect(.None == Fail(void, null).parse(""));
 }
 
-/// A parser that successfully matches nothing
-pub const Nothing = Parser(struct {
-    pub fn parse(input: Input) Result(void) {
-        return Result(void).some({}, input);
-    }
-});
+///
+/// Singleton parser that successfully matches nothing
+///
+pub const Nothing = Pure({});
 
 test "parse nothing" {
     const res = Nothing.parse(ghost);
@@ -142,7 +258,9 @@ test "parse nothing" {
     t.expectEqualSlices(u8, ghost, res.tail().?);
 }
 
-/// A parser that matches nothing if there is nothing left
+///
+/// Singleton parser that only matches the end of input
+///
 pub const End = Parser(struct {
     pub fn parse(input: Input) Result(void) {
         if (input.len != 0) return Result(void).none();
@@ -152,14 +270,28 @@ pub const End = Parser(struct {
 });
 
 test "parse end of input" {
-    t.expectEqual({}, End.parse("").value().?);
+    t.expectEqual(Result(void).some({}, ""), End.parse(""));
     t.expect(.None == End.parse("👻"));
 }
 
-pub fn Not(comptime parser: type) type {
+///
+/// Look-ahead non-matching constructor
+///
+/// Constructs a parser that never consumes input, and negates the result of
+/// the given parser. When it is successful, this parser returns a failed
+/// result without reason, and when it fails, this parser returns a successful
+/// void result.
+///
+/// Arguments:
+///     `P`: the parser to be negated
+///
+/// Returns:
+///     `Parser{ .T = void }`
+///
+pub fn Not(comptime P: type) type {
     return Parser(struct {
         pub fn parse(input: Input) Result(void) {
-            switch (parser.parse(input)) {
+            switch (P.parse(input)) {
                 .Some => return Result(void).none(),
                 .None => return Result(void).some({}, input),
             }
@@ -169,15 +301,63 @@ pub fn Not(comptime parser: type) type {
 
 test "non matching look-ahead" {
     t.expect(.None == Not(Char('👻')).parse(ghost_party));
-    t.expect(.Some == Not(Char('🥳')).parse(ghost_party));
+
+    t.expectEqual(
+        Result(void).some({}, ghost_party),
+        Not(Char('🥳')).parse(ghost_party),
+    );
 }
 
-pub fn Optional(comptime parser: anytype) type {
-    const T = parser.T;
+///
+/// Look-ahead constructor
+///
+/// Constructs a parser that never consumes input, and maps any successful
+/// result of the given parser to void.
+///
+/// Arguments:
+///     `P`: the parser to be tested
+///
+/// Returns:
+///     `Parser{ .T = void }`
+///
+pub fn Try(comptime P: type) type {
+    return Parser(struct {
+        pub fn parse(input: Input) Result(void) {
+            switch (P.parse(input)) {
+                .None => |r| return Result(void).fail(r),
+                .Some => return Result(void).some({}, input),
+            }
+        }
+    });
+}
+
+test "matching look-ahead" {
+    t.expect(.None == Try(Char('🥳')).parse(ghost_party));
+
+    t.expectEqual(
+        Result(void).some({}, ghost_party),
+        Try(Char('👻')).parse(ghost_party),
+    );
+}
+
+///
+/// Optional parser constructor
+///
+/// Constructs a parser that maps the result value type of the given parser
+/// to an optional, and maps any failures to a successful `null` result.
+///
+/// Arguments:
+///     `P`: parser to be made optional
+///
+/// Returns:
+///     `Parser { .T = ?P.T }`
+///
+pub fn Optional(comptime P: anytype) type {
+    const T = P.T;
 
     return Parser(struct {
         pub fn parse(input: Input) Result(?T) {
-            switch (parser.parse(input)) {
+            switch (P.parse(input)) {
                 .Some => |r| return Result(?T).some(r.value, r.tail),
                 .None => return Result(?T).some(null, input),
             }
@@ -191,6 +371,12 @@ test "optional" {
     t.expectEqualSlices(u8, ghost, P.parse(ghost_party).value().?.?);
     t.expect(null == P.parse(party_ghost).value().?);
 }
+
+//------------------------------------------------------------------------------
+//
+//  MARK: Tests for Parser combinators
+//
+//------------------------------------------------------------------------------
 
 test "alternatives" {
     const P = Char('🥳').Alt(Char('👻'));
